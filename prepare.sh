6f010c5fc7cdf77b9f42d8bc7c90426e87ce0640
Jan Sarenik <jajomojo@gmail.com>
2021-11-26 10:09:38 +0000

    prepare.sh: Fix URL for Bulma

--- prepare.sh
+++ prepare.sh
@@ -4,7 +4,7 @@ BV=0.9.3
 BULMA=https://github.com/jgthms/bulma/releases/download/$BV/bulma-$BV.zip
 mkdir tmp 2>/dev/null
 test -r tmp/bulma.zip || {
-  wget -O tmp/bulma.zip 
+  wget -O tmp/bulma.zip $BULMA
 }
 test -r tmp/bulma/css/bulma.css || {
   (cd tmp; unzip bulma.zip)
