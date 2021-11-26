f0bb5ce8e7757be5ab977bd292abd5df501a4b6f
Jan Sarenik <jajomojo@gmail.com>
2021-11-26 09:36:01 +0100

    Add mock if bitcoin is not running

--- run-site.sh
+++ run-site.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 ./prepare.sh
+bitcoin-cli -signet echo "hello world" >/dev/null 2>&1 || {
+  echo Running in mock mode
+  export PATH=$PWD/mock:$PATH
+  export WHERE=$PWD/tmp/faucet
+}
 
 echo Serving at http://localhost:8123
 httpd -f -p 127.0.0.1:8123 -vv
