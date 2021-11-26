41bb4796ee6825d73b9c155586ec97ea97e0a180
Jan Sarenik <jajomojo@gmail.com>
2021-11-26 12:28:14 +0100

    prepare: Less verbose, extract only one file

--- prepare.sh
+++ prepare.sh
@@ -4,10 +4,11 @@ BV=0.9.3
 BULMA=https://github.com/jgthms/bulma/releases/download/$BV/bulma-$BV.zip
 mkdir tmp 2>/dev/null
 test -r tmp/bulma.zip || {
-  wget -O tmp/bulma.zip $BULMA
+  wget -q -O tmp/bulma.zip $BULMA
 }
-test -r tmp/bulma/css/bulma.css || {
-  (cd tmp; unzip bulma.zip)
+USESTYLE=bulma/css/bulma.css
+test -r tmp/$USESTYLE || {
+  (cd tmp; unzip bulma.zip $USESTYLE)
 }
 
 mkdir css 2>/dev/null
