83169d6ba282b5345147671ba5514c7941b4a42e
Jan Sarenik <jajomojo@gmail.com>
2021-11-30 14:32:39 +0100

    bitcoin-cli-signet.sh: Use P2TR address
    This is an example shell script showing how to
    send funds via bitcoin-cli.

--- bitcoin-cli-signet.sh
+++ bitcoin-cli-signet.sh
@@ -1,9 +1,7 @@
 bitcoin-cli -signet -named sendtoaddress \
-  address=tb1qm5m0ju770r36zg0gqhlt3a7yejd45w62czkg62 \
+  address=tb1pc6rlswtdgsadws4ltj7juxgae7mfhm6ytgwwdnfsv8m0wgehaf4sgac7uw \
   amount=0.001 \
-  subtractfeefromamount=true \
+  subtractfeefromamount=false \
   replaceable=true \
   avoid_reuse=false \
   fee_rate=1
-
-#  address=tb1qmnamkevhmz4x4h5sz83v6rhtpaawazm2mvgxqk \
