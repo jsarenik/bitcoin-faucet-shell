83c2058641cc2bd9e4676c9b8d57d254e78d68f9
Jan Sarenik <jajomojo@gmail.com>
2025-02-24 19:42:07 +0000

    sff: Fix signetfaucet.sh spk

--- contrib/sff/signetfaucet.sh
+++ contrib/sff/signetfaucet.sh
@@ -7,10 +7,10 @@ faucetaddr=tb1pupx8xare6jwl87nu058lc4ckqrrd3ugd9q6czxz9my8c49s085pq54ayff
 faucetaddr=tb1p4tp4l6glyr2gs94neqcpr5gha7344nfyznfkc8szkreflscsdkgqsdent4
 faucetaddr=tb1qg3lau83hm9e9tdvzr5k7aqtw3uv0dwkfct4xdn
 
-mkdir -p /tmp/sff
+mkdir -p /tmp/sff /tmp/sff-s2 /tmp/sff-s3 /tmp/sffrest
 addr=${1:-$faucetaddr}
 test -r /tmp/sff/$addr && { echo $addr; exit; }
-spk=$(gai.sh ${addr} | grep -w scriptPubKey | cut -d: -f2 | tr -d ' ",')
+spk=$(gai.sh ${addr} | grep -m1 '^  "scriptPubKey":' | cut -d: -f2 | tr -d ' ",')
 echo "$(hex $((${#spk}/2)) - 2) $spk" | safecat.sh /tmp/sff/$addr
 
 # Just a historical lock, make sure it's not there
