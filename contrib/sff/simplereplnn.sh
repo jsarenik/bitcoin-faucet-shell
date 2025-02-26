d19f701588945f9cb028c8d6e822dcfefb78508b
Jan Sarenik <jajomojo@gmail.com>
2025-02-26 08:48:00 +0000

    sff: make sure we keep to what we mean

--- contrib/sff/simplereplnn.sh
+++ contrib/sff/simplereplnn.sh
@@ -135,8 +135,10 @@ mkdir -p /tmp/sff-s3
 
 d=/tmp/sffrest
 mkdir -p $d
-ls -1 /tmp/sff/ | grep -q . || ls -t1 "$d" 2>/dev/null \
-  | head -n $(((10000-$vsize)/60)) | while read a; do mv "$d/$a" /tmp/sff; done
+ls -1 /tmp/sff/ | grep -q . || {
+  ls -t1 "$d" 2>/dev/null | head -n $(((10000-$vsize)/60)) \
+    | while read a; do mv "$d/$a" /tmp/sff; done
+}
 
 ls /tmp/sff >&2
 find /tmp/sff/ /tmp/sff-s2/ /tmp/sff-s3/ -mindepth 1 2>/dev/null | xargs cat \
