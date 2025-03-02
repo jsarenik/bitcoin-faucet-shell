43bd00e4e60d490f78f2f0fba1e5768efcb0aab3
Jan Sarenik <jajomojo@gmail.com>
2025-03-02 16:19:17 +0000

    sff: refreshsignetwallets.sh for a minimal lnanchor

--- contrib/sff/refreshsignetwallets.sh
+++ contrib/sff/refreshsignetwallets.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#
+
 # This is run @hourly by cron
 
 lock=/tmp/sfflock
@@ -8,14 +8,24 @@ test -d $lock && exit 1
 lock=/tmp/rsw
 mkdir $lock || exit 1
 . $HOME/.profile
+mkdir -p /dev/shm/wallets-signet/lnanchor
 
 cd ~/.bitcoin/signet/wallets
 ash refresh.sh
 ash refr-pokus.sh
 
 cd lnanchor
-lts.sh | awk '{print $1}' | sort -u | rpf.sh
-wv.sh
+#lts.sh | awk '{print $1}' | sort -u | rpf.sh
+#wv.sh
+#fullrpf20.sh
+ulw.sh
+rm /dev/shm/wallets-signet/lnanchor/wallet.dat
+cd ..
+bch.sh createwallet lnanchor true true
+cd lnanchor
+bch.sh importdescriptors '[{"desc": "addr(tb1pfees9rn5nz)#8njps4hg","timestamp": "now"}]'
+. /dev/shm/UpdateTip-signet
+bch.sh rescanblockchain $(($height-20))
 cd ..
 
 rmdir $lock
