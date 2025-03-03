fcccdafab6c0f6ea743c2da2bfd42f4212f8df55
Jan Sarenik <jajomojo@gmail.com>
2025-03-03 18:12:49 +0000

    sff: vsize.sh was a nice education but does not scale

--- contrib/sff/simplereplnn.sh
+++ contrib/sff/simplereplnn.sh
@@ -211,7 +211,7 @@ cat $gmef | grep -w base \
 cat $gmef | grep -w ancestorsize \
   | cut -d: -f2 | tr -dc '[0-9]' | tr -d . | sed 's/^0\+//' \
   | safecat.sh $asf
-vsize=$(cat $hf | vsize.sh | grep .) || myexit 1 "invalid vsize"
+vsize=$(cat $hf | fee.sh | grep .) || myexit 1 "invalid vsize"
 test "$vsize" -lt 99999 || myexit 1 "early TOO BIG vsize $vsize"
 outsum=$(cat $hf | nd-outs.sh | cut -b-16 | ce.sh | fold -w 16 \
 	| while read l; do echo $((0x$l)); done | paste -d+ -s | bc)
