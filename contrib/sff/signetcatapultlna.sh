1578925c18d1652d52abe6baece09c3e6527b0a0
Jan Sarenik <jajomojo@gmail.com>
2025-03-06 13:03:46 +0000

    sff: signetcatapultlna.sh use at least 2 valid inputs

--- contrib/sff/signetcatapultlna.sh
+++ contrib/sff/signetcatapultlna.sh
@@ -2,16 +2,29 @@
 net=$(hnet.sh)
 tmp=$(mktemp /dev/shm/catapult-$net-$$-XXXXXX)
 
+myexit() {
+  ret=$?
+  rm -rf $tmp > /dev/null
+  exit $ret
+}
+
 cd ~/.bitcoin/signet/wallets/lnanchor
 
 #read -r last < /tmp/signetlast
 #genin.sh $last 1 | safecat.sh $tmp
 
 #throw spam
-list.sh | sort -n -k3 | safecat.sh $tmp
+list.sh | grep -v "240 0 false$" | safecat.sh $tmp
+test $(wc -l < $tmp) -ge 2 || myexit
+test -s $tmp || myexit
+
+#list.sh | sort -n -k3 | safecat.sh $tmp
 #list.sh | awk '($3<0.0001){print}' | sort -n -k3 | safecat.sh $tmp
 #list.sh | grep " false$" | awk '($3<0.01){print}' | safecat.sh $tmp
 grep -q . $tmp && {
+awklist-allfeemin.sh < $tmp | crt.sh | sert.sh
+myexit
+
 while read line
 do
 echo $line >&2
@@ -21,5 +34,3 @@ echo $line | awklist-allfee.sh | mktx.sh | crt.sh \
   | sert.sh >/dev/null 2>&1
 done < $tmp
 }
-
-rm -rf $tmp > /dev/null
