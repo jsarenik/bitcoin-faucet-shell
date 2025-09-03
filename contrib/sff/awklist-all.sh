2ae71e3931ac11f91546d40a03bfb732ed2f7a01
Jan Sarenik <jajomojo@gmail.com>
2025-09-03 10:03:35 +0000

    sff: awklist-all.sh sync
    Soon to be put into a function.

--- contrib/sff/awklist-all.sh
+++ contrib/sff/awklist-all.sh
@@ -6,17 +6,21 @@ addrout=tb1pupx8xare6jwl87nu058lc4ckqrrd3ugd9q6czxz9my8c49s085pq54ayff
 addrout=tb1qg3lau83hm9e9tdvzr5k7aqtw3uv0dwkfct4xdn
 faucet=tb1qg3lau83hm9e9tdvzr5k7aqtw3uv0dwkfct4xdn # signet faucet
 faucet=tb1p4tp4l6glyr2gs94neqcpr5gha7344nfyznfkc8szkreflscsdkgqsdent4 # signet faucet
-fee=141
+vsize=141
 add=0
-gmm=$(gmm-gen.sh)
+gmm=$(gmm.sh)
 gmm=${gmm:-1000}
-feerate=$(($gmm*11/3))
-test "$1" = "-f" && { fee=$2; shift 2; }
-test "$1" = "-fr" && { feerate=1000; shift; }
+#gmm=$(($gmm*4))
+#test $gmm -gt 5000 && gmm=5000
+#feerate=$(($gmm*42/3))
+feerate=$(($gmm*2*16/13))
+test "$1" = "-f" && { vsize=$2; shift 2; }
+test "$1" = "-fr" && { feerate=$gmm; shift; }
+test "$1" = "-fm" && { feerate=1000; shift; }
 test "$1" = "-o" && { faucet=$2; shift 2; }
 test "$1" = "-d" && { faucet=$2; shift 2; }
 #amount=$(echo 6${RANDOM}${RANDOM} | cut -b-5)
 #amount=$(($amount+$RANDOM))
 
-awk "{sum+=\$3; print \$1\":\"\$2} END {sum-=(($fee*$feerate/1000)+$add)/100000000; printf(\"$faucet,%.8f\n\", sum); }"
+awk "{sum+=\$3; print \$1\":\"\$2} END {sum-=(($vsize*$feerate/1000)+$add)/100000000; printf(\"$faucet,%.8f\n\", sum); }"
 #${1}
