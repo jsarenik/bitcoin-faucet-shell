7b0aecd331afffdbb16841d9cbd6894a0d66c0de
Jan Sarenik <jajomojo@gmail.com>
2025-10-09 07:53:36 +0000

    sff: 25 one by one in simplereplnn
    Working, but not pitch-perfect yet.

--- contrib/sff/simplereplnn.sh
+++ contrib/sff/simplereplnn.sh
@@ -14,37 +14,6 @@ bch.sh echo hello | grep -q . || myexit 1 "early bitcoin-cli echo hello"
 ## are we online?
 ping -qc1 1.1.1.1 2>/dev/null >&2 || myexit 1 offline
 
-### ############# DO THE 25 ###################
-###
-### do the chain of 25-in-mempool transactions
-###
-### ###########################################
-dothetf() {
-isoldb || myexit 1 "isoldb in dothetf"
-
-: > $lpr
-for i in $(seq ${1:-25})
-do
-  test -s "$lpr" && {
-  until
-    dolisto
-    ! cmp $l $lpr
-  do
-    usleep 21
-  done
-  }
-  cd $wd
-  fee=$(awklist-all.sh -d $otra < $l \
-    | mktx.sh | crt.sh | srt.sh | fee.sh)
-  awklist-all.sh -f $fee -d $otra < $l  \
-    | mktx.sh | crt.sh | srt.sh | safecat.sh $shf
-  sertl <$shf
-  grep -q . $sfl || break
-  cp $l $lpr
-done
-myexit 1 dothetf
-}
-
 cleanupr() {
   intx=$1
 
@@ -98,8 +67,6 @@ isoldb || {
   sertl <$shf
   read -r last < $sfl
 
-  dothetf
-
   cd $myp
   catapultleftovers
 
@@ -125,8 +92,13 @@ skipround() {
 
   cleanupr $txid
 
+  dolisto
   cd $wd
-  mylist | grep " 0 true$" | awklist-all.sh -f 50000 | mktx.sh | crt.sh | srt.sh | sertl
+  fee=$(awklist-all.sh -d $dent < $l \
+    | mktx.sh | crt.sh | srt.sh | fee.sh)
+  awklist-all.sh -f $fee -d $dent < $l  \
+    | mktx.sh | crt.sh | srt.sh | safecat.sh $shf
+  sertl <$shf
   cd $myp
 }
 
@@ -138,25 +110,31 @@ txid=${tx%% *}
 test "$txid" = "" && myexit 1 "empty TXID"
 echo "$txid" | grep -E '[0-9a-f]{64}' || myexit 1 "strange TXID"
 
+test "$ancestorcount" = "25" || {
+  cleanupr $txid
+  skipround
+
+  dolisto
+
+  tx=$(cat $l | head -1 | grep .) # || myexit 1 "skipround newblock tx"
+  txid=${tx%% *}
+  gengmep
+  test -s "$gmep" || myexit 1 "missing gmep II"
+  . $gmep
+}
+
 gengmep
-test -s "$gmef" || dothetf
+test -s "$gmep" || myexit 1 "missing gmep"
 # sets vsize weight time height descendantcount descendantsize
 # ancestorcount ancestorsize wtxid base modified ancestor descendant
 . $gmep
-test "$ancestorcount" = "25" || {
-  if
-    test "$ancestorcount" = "1"
-  then
-    dothetf $((25-$ancestorcount))
-  else
-    test "$ancestorcount" -lt "24" && skipround
-    myexit 1 skipround
-  fi
-}
-test "$descendantcount" = "1" || myexit 1 "descendantcount"
 test "$vsize" -lt 98299 || myexit 1 "early TOO BIG vsize $vsize"
+
 depends=$(jq -r .depends[0] < $gmef)
 dce=$(echo $depends | ce.sh)
+
+#depends=$txid
+#dce=$(echo $depends | ce.sh)
 
 value=$(getamount)
 outsum=$(($value-${base:-0}))
