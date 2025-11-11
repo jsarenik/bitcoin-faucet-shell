e49d5cc61c1f744650aa69bc3a1bbeb32c921b0f
Jan Sarenik <jajomojo@gmail.com>
2025-11-11 14:47:41 +0000

    sff: orl.sh: support greater lengths

--- contrib/sff/orl.sh
+++ contrib/sff/orl.sh
@@ -6,4 +6,5 @@ msg=$(printf "$msg" | xxd -p)
 lend=$((${#msg}/2))
 len=$(printf "%02x" $lend)
 leno=$(printf "%02x" $(($lend+2)))
+test $lend -ge 76 && { len="4c$len"; leno=$(printf "%02x" $((0x$leno+1))); }
 echo 0000000000000000 $leno 6a$len $msg
