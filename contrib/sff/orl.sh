195fd8485e4fc7b5cc8acb8baa80f0feb20063fa
Jan Sarenik <jajomojo@gmail.com>
2025-11-13 10:56:21 +0000

    sff: orl.sh: faster and wider

--- contrib/sff/orl.sh
+++ contrib/sff/orl.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
 
 msg=${1:"test"}
-test "$1" = "" && read -r msg
+msgf=/tmp/orl-msg
 
-msg=$(printf "$msg" | xxd -p)
-lend=$((${#msg}/2))
+if
+  test "$1" = ""
+then
+  cat > $msgf
+else
+  printf "$msg" | xxd -p > $msgf
+fi
+
+lend=$(stat -c %s $msgf)
+
 len=$(printf "%02x" $lend)
 leno=$(printf "%02x" $(($lend+2)))
+
 test $lend -ge 76 && { len="4c$len"; leno=$(printf "%02x" $((0x$leno+1))); }
-echo 0000000000000000 $leno 6a$len $msg
+test $lend -ge 253 && {
+  len=$(printf "%04x" $lend)
+  leno=$(printf "%04x" $(($lend+3)))
+  len=$(echo $len | ce.sh)
+  len="4d$len"
+  leno=$(printf "%04x" $((0x$leno+1)) | ce.sh)
+  leno="fd$leno"
+}
+echo 0000000000000000 $leno 6a$len
+xxd -p < $msgf
